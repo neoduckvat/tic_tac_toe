a9b6775d5098b29257395f2cb01c579e0b7106fc
Neoduckvat <neoduckvat@gmail.com>
2025-10-02 23:36:48 +0300

    Import the module into the game

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
-# Объявить класс.
 class Board:
-    # Инициализировать игровое поле - список списков с пробелами.
-    # Пробелы - это пустые клетки.
+
+    from parts import Board
+    
     def __init__(self):
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
 
